a11c08819987674e8b82f476f4482fd2fb79e345
Kevin Pudlo <pudlok@gmail.com>
2024-10-29 18:01:53 -0400

    Add Vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'luxibay/oxus',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c9d667fa959963af0d16393ceb2ea880cf89d333',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luxibay/oxus' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c9d667fa959963af0d16393ceb2ea880cf89d333',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptrt/admin-notices' => array(
+            'pretty_version' => 'v1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => '3904e0dc087b48289056a77eea3d1dab4ef0dde1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wptrt/admin-notices',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
