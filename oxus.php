0c31c9a60d35652cefc3a22cdaf5188571b6564a
Kevin Pudlo <pudlok@gmail.com>
2025-10-17 06:49:10 -0400

    Ensured Licensing folder was included

--- oxus.php
+++ oxus.php
@@ -6,7 +6,7 @@ Plugin URI: https://nauticode.luxibay.com/oxus
 Author: Nauticode by Luxibay
 Author URI: https://nauticode.luxibay.com/
 Description: Oxus provides integrative support for WPML when using the Classic Oxygen plugin.
-Version: 0.9.1
+Version: 0.9.2
 Requires Plugins: sitepress-multilingual-cms, oxygen
 Text Domain: oxus
 */
@@ -26,10 +26,33 @@ function oxus_cast()
     $oxus = new Oxus_Connect();
 }
 
+/**
+ * Display admin notice when the licensing bundle is missing.
+ *
+ * @return void
+ */
+function oxus_display_missing_licensing_notice()
+{
+    ?>
+    <div class="notice notice-error">
+        <p>
+            <strong>Oxus:</strong> The SureCart licensing library is missing. Please reinstall or reupload the plugin so the <code>licensing/src</code> directory is present.
+        </p>
+    </div>
+    <?php
+}
+
 
 add_action('init', function(){
+	$client_path = __DIR__ . '/licensing/src/Client.php';
+
+	if ( ! file_exists( $client_path ) ) {
+		add_action( 'admin_notices', 'oxus_display_missing_licensing_notice' );
+		return;
+	}
+
 	if ( ! class_exists( 'SureCart\Licensing\Client' ) ) {
-		require_once __DIR__ . '/licensing/src/Client.php';
+		require_once $client_path;
 	}
 	
 	// initialize client with your plugin name and your public token.
